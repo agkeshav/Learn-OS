d038cf6270646d93f269fe3169b9dffac70b1cb5
Vasu_Bhanderi <vbbhanderi1312@gmail.com>
2023-01-20 01:34:07 +0530

    App.js modified to RN navigation v5

--- App.js
+++ App.js
@@ -2,12 +2,24 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
-//Are you able to change it?
+import { createStackNavigator } from "@react-navigation/stack";
+
 import LearnCommandsScreen from "./src/Screens/LearnCommandsScreen";
 import Logout from "./src/Screens/Logout";
-import HomeScreen from './src/Screens/HomeScreen';
+import HomeScreen from "./src/Screens/HomeScreen";
 
 const Drawer = createDrawerNavigator();
+const Stack = createStackNavigator();
+
+const DrawerStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Learn Commands" component={LearnCommandsScreen} />
+      <Stack.Screen name="Accounts" component={Logout} />
+    </Stack.Navigator>
+  );
+};
 
 export default function App() {
   return (
