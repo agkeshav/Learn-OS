12f2357c2fb305ba8ba02eac7db64591ab515d15
Vasu_Bhanderi <vbbhanderi1312@gmail.com>
2023-01-22 00:34:18 +0530

    Basic Command Line Demonstration Setup Done

--- App.js
+++ App.js
@@ -1,34 +1,38 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { Provider as CommandsProvider } from "./src/context/commandsContext";
 
 import LearnCommandsScreen from "./src/Screens/LearnCommandsScreen";
-import Logout from "./src/Screens/Logout";
 import HomeScreen from "./src/Screens/HomeScreen";
 
 const Drawer = createDrawerNavigator();
 const Stack = createStackNavigator();
 
-const DrawerStack = () => {
+// const DrawerStack = () => {
+//   return (
+//     <Stack.Navigator>
+//       <Stack.Screen name="Home" component={HomeScreen} />
+//       <Stack.Screen name="Learn Commands" component={LearnCommandsScreen} />
+//       <Stack.Screen name="Accounts" component={Logout} />
+//     </Stack.Navigator>
+//   );
+// };
+
+const App = () => {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="Home" component={HomeScreen} />
-      <Stack.Screen name="Learn Commands" component={LearnCommandsScreen} />
-      <Stack.Screen name="Accounts" component={Logout} />
-    </Stack.Navigator>
+    <CommandsProvider>
+      <NavigationContainer>
+        <Drawer.Navigator initialRouteName="Home">
+          <Drawer.Screen name="Home" component={HomeScreen} />
+          <Drawer.Screen
+            name="Learn Commands"
+            component={LearnCommandsScreen}
+          />
+        </Drawer.Navigator>
+      </NavigationContainer>
+    </CommandsProvider>
   );
 };
 
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={HomeScreen} />
-        <Drawer.Screen name="Learn Commands" component={LearnCommandsScreen} />
-        <Drawer.Screen name="Accounts" component={Logout} />
-      </Drawer.Navigator>
-    </NavigationContainer>
-  );
-}
+export default App;
---
+++ src/context/commandsContext.js
@@ -0,0 +1,322 @@
+import createDataContext from "./createDataContext";
+import { useState } from "react";
+
+const commandsReducer = (state, action) => {
+  switch (action.type) {
+    case "mkdir":
+      var i = state.dirs.length;
+      var found = false;
+      while (i--) {
+        if (action.payload == state.dirs[i].name) {
+          found = true;
+          break;
+        }
+      }
+
+      if (!found) {
+        var new_dirs = [];
+        if (state.dirs.length > 0) {
+          new_dirs = [
+            ...state.dirs,
+            {
+              name: action.payload,
+              id: state.dirs[state.dirs.length - 1].id + 1,
+              files: [],
+              type: "dir",
+            },
+          ];
+        } else {
+          new_dirs = [
+            ...state.dirs,
+            {
+              name: action.payload,
+              id: 1,
+              files: [],
+              type: "dir",
+            },
+          ];
+        }
+        state.dirs = new_dirs;
+        state.errorMsg = null;
+        state.message = `${action.payload} created under the ${state.currentDir} directory`;
+      } else {
+        state.errorMsg = `${action.payload} already exists!`;
+        state.message = null;
+      }
+
+      state.showList = false;
+      state.showCurrentDir = false;
+      return state;
+
+    case "rmdir":
+      var i = state.dirs.length;
+      var found = false;
+      while (i--) {
+        if (state.dirs[i].name === action.payload) {
+          state.dirs.splice(i, 1);
+          found = true;
+          state.message = `${action.payload} removed successfully`;
+        }
+      }
+      if (!found) {
+        state.errorMsg = `${action.payload} directory not found`;
+      }
+      state.showList = false;
+      state.showCurrentDir = false;
+      return state;
+
+    case "ls":
+      if (state.currentDir == "root") {
+        state.list = state.dirs.concat(state.files);
+      } else {
+        var dir_id;
+        var i = state.dirs.length;
+        while (i--) {
+          if (state.dirs[i].name == state.currentDir) {
+            dir_id = i;
+          }
+        }
+
+        state.list = state.dirs[dir_id].files;
+      }
+
+      if (state.list.length > 0) {
+        state.showList = true;
+        state.showCurrentDir = false;
+        state.errorMsg = null;
+        state.message = null;
+      } else {
+        state.errorMsg = "No content found!";
+        state.showList = false;
+        state.message = null;
+        state.showCurrentDir = false;
+      }
+
+      return state;
+
+    case "pwd":
+      state.showList = false;
+      state.errorMsg = null;
+      state.showCurrentDir = true;
+      state.message = null;
+      return state;
+
+    case "cd":
+      if (action.payload == "root" || action.payload == "..") {
+        state.currentDir = "root";
+        state.message = "current directory is shifted to root";
+      } else {
+        var i = state.dirs.length;
+        var found = false;
+        while (i--) {
+          if (state.dirs[i].name === action.payload) {
+            state.currentDir = action.payload;
+            found = true;
+            break;
+          }
+        }
+        if (!found) {
+          state.errorMsg = "Directory not found!";
+          state.message = null;
+        } else {
+          state.message = `current directory is shifted to ${action.payload}`;
+          state.errorMsg = null;
+        }
+      }
+
+      state.showList = false;
+      state.showCurrentDir = false;
+
+      return state;
+
+    case "clear":
+      state.showCurrentDir = false;
+      state.showList = false;
+      state.errorMsg = null;
+      state.message = null;
+      return state;
+
+    case "touch":
+      if (state.currentDir == "root") {
+        var i = state.files.length;
+        var found = false;
+        while (i--) {
+          if (action.payload == state.files[i].name) {
+            found = true;
+            break;
+          }
+        }
+        if (!found) {
+          var new_files;
+          if (state.files.length > 0) {
+            new_files = [
+              ...state.files,
+              {
+                name: action.payload,
+                id: state.files[state.files.length - 1].id + 1,
+                content: "",
+                type: "file",
+              },
+            ];
+          } else {
+            new_files = [
+              ...state.files,
+              {
+                name: action.payload,
+                id: 1,
+                content: "",
+                type: "file",
+              },
+            ];
+          }
+          state.errorMsg = null;
+          state.message = `${action.payload} file is created in the ${state.currentDir} directory`;
+          state.files = new_files;
+        } else {
+          state.errorMsg = `${action.payload} already exists`;
+          state.message = null;
+        }
+      } else {
+        var dir_id = 0;
+        var i = state.dirs.length;
+        var new_files;
+        while (i--) {
+          if (state.dirs[i].name == state.currentDir) {
+            dir_id = i;
+            break;
+          }
+        }
+
+        var j = state.dirs[dir_id].files.length;
+        var found = false;
+        while (j--) {
+          if (action.payload == state.dirs[dir_id].files[j].name) {
+            found = true;
+            break;
+          }
+        }
+
+        if (!found) {
+          if (state.dirs[dir_id].files.length > 0) {
+            new_files = [
+              ...state.dirs[dir_id].files,
+              {
+                name: action.payload,
+                id:
+                  state.dirs[dir_id].files[state.dirs[dir_id].files.length - 1]
+                    .id + 1,
+                content: "",
+                type: "file",
+              },
+            ];
+          } else {
+            new_files = [
+              ...state.dirs[dir_id].files,
+              {
+                name: action.payload,
+                id: 1,
+                content: "",
+                type: "file",
+              },
+            ];
+          }
+          state.errorMsg = null;
+          state.message = `${action.payload} file is created in the ${state.currentDir} directory`;
+          state.dirs[dir_id].files = new_files;
+        } else {
+          state.errorMsg = `${action.payload} already exists`;
+          state.message = null;
+        }
+      }
+
+      state.showList = false;
+      state.showCurrentDir = false;
+      return state;
+
+    case "rm":
+      if (state.currentDir == "root") {
+        var i = state.files.length;
+        var found = false;
+        while (i--) {
+          if (state.files[i].name === action.payload) {
+            state.files.splice(i, 1);
+            found = true;
+            state.message = `${action.payload} removed successfully`;
+          }
+        }
+        if (!found) {
+          state.errorMsg = `${action.payload} file not found`;
+        } else {
+          state.errorMsg = null;
+        }
+      } else {
+        var dir_id;
+        var i = state.dirs.length;
+        var new_files;
+        while (i--) {
+          if (state.dirs[i].name == state.currentDir) {
+            dir_id = i;
+            break;
+          }
+        }
+
+        var i = state.dirs[dir_id].files.length;
+        var found = false;
+        while (i--) {
+          if (state.dirs[dir_id].files[i].name === action.payload) {
+            state.dirs[dir_id].files.splice(i, 1);
+            found = true;
+            state.message = `${action.payload} removed successfully`;
+          }
+        }
+        if (!found) {
+          state.errorMsg = `${action.payload} file not found`;
+        } else {
+          state.errorMsg = null;
+        }
+      }
+      state.showList = false;
+      state.showCurrentDir = false;
+      return state;
+
+    default:
+      state.showList = false;
+      state.showCurrentDir = false;
+      state.errorMsg = `Invalid command!`;
+      state.message = null;
+      return state;
+  }
+};
+
+const execute = (dispatch) => (input) => {
+  if (input == "ls") {
+    dispatch({ type: "ls" });
+  } else if (input == "pwd") {
+    dispatch({ type: "pwd" });
+  } else if (input == "clear" || input == "cls") {
+    dispatch({ type: "clear" });
+  } else {
+    const command = input.split(" ")[0];
+    const argument = input.replace(`${command} `, "");
+    if (argument !== command) {
+      dispatch({ type: command, payload: argument });
+    }
+  }
+};
+
+export const { Context, Provider } = createDataContext(
+  commandsReducer,
+  { execute },
+  {
+    name: "root",
+    id: 1,
+    dirs: [],
+    files: [],
+    showList: false,
+    currentDir: "root",
+    showCurrentDir: false,
+    errorMsg: null,
+    message: null,
+    list: [],
+  }
+);
